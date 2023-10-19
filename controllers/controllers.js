fcbde814dd00f4e81872876899770a9edfa04fdc
Annaivv <annashalya@gmail.com>
2023-10-19 14:43:12 +0300

    try-catch for getDeals added

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,9 +2,13 @@ const db = require("../db");
 
 class DealsController {
   async getDeals(req, res) {
-    const deals = await db.query("SELECT * FROM deals");
-    console.log(deals.rows);
-    res.json(deals.rows);
+    try {
+      const deals = await db.query("SELECT * FROM deals");
+      console.log(deals.rows);
+      res.json(deals.rows);
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 }
 

cfe4d1f8c042794e771f8c221a0e0d9fce8cf260
Annaivv <annashalya@gmail.com>
2023-10-19 16:00:02 +0300

    SSL_FLAG

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Pool = require("pg").Pool;
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, SSL_FLAG } =
+  process.env;
 
 const pool = new Pool({
   user: DB_USER,
@@ -8,7 +9,7 @@ const pool = new Pool({
   host: DB_HOST,
   port: DB_PORT,
   database: DB_NAME,
-  ssl: true,
+  ssl: SSL_FLAG,
 });
 
 module.exports = pool;
